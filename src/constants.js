cea2ff38ba1dae390afc8cc9c085e012ef793b7b
Taha Baran <tahabaranakti10@gmail.com>
2024-07-27 14:42:10 +0300

    add: initialize files

---
+++ src/constants.js
@@ -0,0 +1,36 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. These days I am mostly interested in Theoretical courses and C++.
+  I have done projects with languages like Java, Python and JavaScript.
+  Anyway regardless of the language, I just like programming.
+  So please don't give me feedback that it is not good, it is not right to deal with so many things.
+  I JUST LOVE INFORMATION TECHNOLOGY!
+  Here is my <a href="https://github.com/Tahabarannn" target="_blank">Github</a>!`,
+
+  "cs-degree": `This is my CS master's degree. I hung it on the wall because I'm proud of it. 
+  It was a very theoretical degree but I think it gave me a good foundation.`,
+
+  "sofa-table": `That's my sofa. I like to relax here and read books and watch videos on topics like Psychology, Physiology and Physics.
+  I also actively do sports to maintain my health. I think it's important to have a balance between mental and physical health.`,
+
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@Sadievrenseker_BK" target="_blank">Bilgisayar Kavramlari</a>, 
+   <a href="https://www.youtube.com/@freecodecamp" target="_blank">FreeCodeCamp.org</a>,
+  <a href="https://www.youtube.com/@TCMSecurityAcademy" target="_blank">The Cyber Mentor</a> 
+   and <a href="https://www.youtube.com/@FolksDev" target="_blank">FolksDev</a>!`,
+
+  bed: `I sleep here, lying in bed, and great ideas come to me. When an idea comes up, I often use apps like Notion and Obsidian to write them down, 
+  otherwise I can't sleep because my mental energy is depleted.`,
+
+  resume: `This is my desk and on it is my resume. 
+  <a href="https://github.com/Tahabarannn/Portfolio/blob/main/public/TahaBaran_Cv.pdf" target="_blank">
+  Check it out?</a>
+  Contact me at tahabarann@hotmail.com if you have any interesting job opportunities!`,
+
+  library: `I have books from all genres on my shelves, Psychology, Literature, Business, Technology and Philosophy are the genres I read the most. 
+  Nowadays, I read mostly simple German, French and Russian books, trying to get fluency in languages 
+  that I learned at a basic level in school and did not continue. `,
+
+  exit: `If you want to exit My Portfolio, just close the tab. :)`,
+};
